3a1ba2ee4c4b7c14afb30a145bbe82989d879e16
Neha Vyas <guddu23vyas@gmail.com>
2012-03-06 13:31:55 -0800

    Init commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Coen280Try2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coen280Try2::Application.config.secret_token = '28e3b19a1356bca4cb64fde5d09fba99fa93469e9cee508fe5665190b68d1f4b693cff39310ffa155cadcd765627a2f7befdb67df2e541882ecb0eeb43717311'
