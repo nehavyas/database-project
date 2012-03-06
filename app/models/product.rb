0e8c611922a6448fed35d4baf8259201ccfe90d0
Neha Vyas <guddu23vyas@gmail.com>
2012-03-06 15:37:18 -0800

    added cart and line items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product < ActiveRecord::Base
   default_scope :order => 'name'
   belongs_to :category
+  has_many :line_items
+
   before_save :ensure_valid_values
+  before_destroy :ensure_not_referenced_by_any_line_item
 
   private
     def ensure_valid_values
@@ -21,5 +24,15 @@ class Product < ActiveRecord::Base
       end
 
       return true
+    end
+
+    #ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+         errors.add(:base, 'Line Items present')
+         return false
+      end
     end
 end
